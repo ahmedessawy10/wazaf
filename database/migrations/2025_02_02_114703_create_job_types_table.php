384b094e00e203220e4c8548a510e82a5ec9ff3e
hadeerahme <hadeer123www.com@gmail.com>
2025-02-02 17:35:20 +0200

    save

--- database/migrations/2025_02_02_114703_create_job_types_table.php
+++ database/migrations/2025_02_02_114703_create_job_types_table.php
@@ -9,17 +9,17 @@ return new class extends Migration {
     {
         Schema::create('job_types', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD:database/migrations/2025_02_02_114703_create_job_types_table.php
+
             $table->string('name');
             $table->boolean('status')->default(true);
-=======
+
             $table->string('app_name')->default('Wazaf');
             $table->string('app_email')->default('wazaf@example.com');
             $table->string('logo')->nullable();
             $table->string('favicon')->nullable();
             $table->string('theme_color')->default('#000000');
 
->>>>>>> e3df90c78be93e110c99a167e01e6f7b206a2dfc:database/migrations/2025_01_21_165610_create_app_settings_table.php
+
             $table->timestamps();
         });
     }
